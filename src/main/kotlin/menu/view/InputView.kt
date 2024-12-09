093235885997a21249bf95245bc224d042eac308
m6z1 <sonmyungj1zz@gmail.com>
2024-12-09 17:58:44 +0900

    feat: 제공된 출력 안내 형식으로 수정(코치 이름 및 못 먹는 음식)

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun getCoachNames(): List<String> {
+        println("코치의 이름을 입력해 주세요. (, 로 구분)")
         val coaches = Console.readLine().split(",")
         require(coaches.distinct().count() == coaches.count()) { "[ERROR] 코치 이름은 중복될 수 없습니다." }
         coaches.forEach { coach ->
@@ -15,6 +16,7 @@ class InputView {
     }
 
     fun getCantEatFoodByCoach(coachName: String): List<String>? {
+        println()
         println("$coachName(이)가 못 먹는 메뉴를 입력해 주세요.")
         val cantEatFoods = Console.readLine()
         if (cantEatFoods.isBlank()) return null
