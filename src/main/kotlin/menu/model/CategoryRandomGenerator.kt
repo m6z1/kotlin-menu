3f71722fc9529d63269f81f978eadf138d00be3f
m6z1 <sonmyungj1zz@gmail.com>
2024-12-09 21:10:05 +0900

    fix: 범위 맞추기용 카테고리 요소 추가

--- src/main/kotlin/menu/model/CategoryRandomGenerator.kt
+++ src/main/kotlin/menu/model/CategoryRandomGenerator.kt
@@ -3,11 +3,11 @@ package menu.model
 import camp.nextstep.edu.missionutils.Randoms
 
 class CategoryRandomGenerator {
-    private val categories: List<String> = listOf("일식", "한식", "중식", "아시안", "양식")
+    private val categories: List<String> = listOf("낫싱", "일식", "한식", "중식", "아시안", "양식")
     private val categoriesByCount: MutableList<String> = mutableListOf()
 
     fun generateCategory(): String {
-        val category: String = categories[Randoms.pickNumberInRange(1 - 1, 5 - 1)]
+        val category: String = categories[Randoms.pickNumberInRange(1, 5)]
 
         if (isValidCategory(category)) {
             categoriesByCount.add(category)
