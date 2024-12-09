5368d7fb9a1709ea22590022e744a06730175419
m6z1 <sonmyungj1zz@gmail.com>
2024-12-09 21:09:52 +0900

    build: 테스트 위한 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.21"
 }
+group = "camp.nextstep.edu"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -9,6 +11,9 @@ repositories {
 
 dependencies {
     implementation("com.github.woowacourse-projects:mission-utils:1.2.0")
+    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 java {
