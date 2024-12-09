df859eb2655a3170f3a00ed89b636b7c587eb1b5
m6z1 <sonmyungj1zz@gmail.com>
2024-12-09 17:17:15 +0900

    feat: 코치 인원 예외 처리

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -10,5 +10,6 @@ class InputView {
         coaches.forEach { coach ->
             require(coach.length in 2..4) { "[ERROR] 코치 이름은 2~4글자만 가능합니다." }
         }
+        require(coaches.count() in 2..5) { "[ERROR] 코치는 2~5명까지만 입력할 수 있습니다." }
     }
 }
