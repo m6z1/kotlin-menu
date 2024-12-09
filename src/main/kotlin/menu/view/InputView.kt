e9f2a10af54c2c09ddd2b8f26c6e0b542ac45e0d
m6z1 <sonmyungj1zz@gmail.com>
2024-12-09 17:17:05 +0900

    feat: 코치 이름 중복 체크 예외 처리

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -7,5 +7,8 @@ class InputView {
     fun getCoachNames() {
         val coaches = Console.readLine().split(",")
         require(coaches.distinct().count() == coaches.count()) { "[ERROR] 코치 이름은 중복될 수 없습니다." }
+        coaches.forEach { coach ->
+            require(coach.length in 2..4) { "[ERROR] 코치 이름은 2~4글자만 가능합니다." }
+        }
     }
 }
